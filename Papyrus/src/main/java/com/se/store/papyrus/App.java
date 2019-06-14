17d704ab2a43a79d8adeb08119d6b80126956f17
Golam Alim <Golam Alim@hx-laptop>
2019-06-14 10:09:58 +0600

    Init

---
+++ Papyrus/src/main/java/com/se/store/papyrus/App.java
@@ -0,0 +1,16 @@
+package com.se.store.papyrus;
+
+/**
+ * Project Name: Papyrus
+ * Application For: Book selling shop management
+ * Create Date: 14-Jun-2019 08:05:00AM
+ * Created By: Golam S. Alim
+ * Project Name History: https://en.wikibooks.org/wiki/Papermaking/History_of_paper
+ */
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.exit(0);
+	}
+}
